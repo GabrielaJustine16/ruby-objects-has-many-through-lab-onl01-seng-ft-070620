a7df3a0c48cbf2da4bca3299808a1d9957da7769
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-22 19:17:44 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,25 +1,24 @@
 class Patient
-  attr_reader :name
-  @@all = []
-
+  attr_accessor :name 
+  
   def initialize(name)
-    @name = name
-    @@all << self
+    @name=name
   end
-
-  def self.all
-    @@all
-  end
-
-  def new_appointment(doctor, date)
+  
+   def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
   end
 
   def appointments
-    Appointment.all.select { |a| a.patient == self  }
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
   end
 
   def doctors
-    appointments.map { |b| b.doctor }
+    appointments.map do |appointment|
+      appointment.doctor
+    end
   end
+  
 end
