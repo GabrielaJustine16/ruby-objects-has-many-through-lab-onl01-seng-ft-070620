582de6cf9c15c669e85dc531dd980e818c94d0e3
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-22 19:32:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,25 +1,29 @@
-class Artist 
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-    @songs = [] 
-  end
-  
-  def add_song(song)
-    @songs << song 
-    song.artist = self
-  end
-  
-  def songs 
-    @songs 
-  end 
-  
-  def genres 
-    @songs.map do |song|
-      song.genre
+class Artist
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
     end
-  end
-  
-  
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def genres
+        self.songs.map {|song| song.genre}
+    end
+
+    
+
 end
