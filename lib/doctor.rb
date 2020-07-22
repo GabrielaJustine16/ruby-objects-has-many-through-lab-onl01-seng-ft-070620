880992caa31822c516b5d2b6e253cba20d8969a8
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-22 19:18:41 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,24 +1,27 @@
 class Doctor
-  attr_reader :name
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+  @@all=[]
 
   def self.all
     @@all
   end
-  def new_appointment(patient, date)
-    Appointment.new(date, patient, self)
+
+  attr_accessor :name, :appointments, :patient
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
   end
 
-  def appointments
-    Appointment.all.select { |a| a.doctor == self  }
+  def new_appointment(date, patient)
+     appointment = Appointment.new(patient, date, self)
+     @appointments << appointment
+     appointment
   end
 
   def patients
-    appointments.map { |appointment| appointment.patient  }
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
   end
 end
