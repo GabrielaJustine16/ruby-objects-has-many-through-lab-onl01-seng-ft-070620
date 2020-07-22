2293fcfbf6013576027a68a497d01017b0184ce8
Gabriela johnson <johnsongabriela12@gmail.com>
2020-07-22 19:33:32 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,19 +1,19 @@
 class Appointment
-  attr_accessor :date, :doctor, :patient
-  
-  def initialize(date, doctor)
-    @date = date 
-    @doctor = doctor 
-    doctor.appointments << self
-  end
-  
-  def patient
-    self.patient = @patient
-  end
-  
-  def doctor 
-    self.doctor = @doctor
-  end
-    
-    
+
+    attr_accessor :date, :patient, :doctor
+
+    @@all = []
+
+    def initialize(date, patient, doctor)
+        @date = date
+        @patient = patient
+        @doctor = doctor
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+
 end
